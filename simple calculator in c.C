ef968e669551013f47cbbafb84e78ff271be67d5
Bjayakrishna125 <98174819+Bjayakrishna125@users.noreply.github.com>
2022-04-26 14:02:43 +0530

    Update simple calculator in c.C

--- simple calculator in c.C
+++ simple calculator in c.C
@@ -1,31 +1,30 @@
 #include <stdio.h>
-#include <conio.h>
 
 int main() {
 
   char sign;
-  double first, second;
+  int first, second;
   clrscr();
   printf("Enter an operator (+, -, *, /): ");
   scanf("%c", &sign);
   printf("Enter two operands: ");
-  scanf("%lf %lf", &first, &second);
+  scanf("%d %d", &first, &second);
 
   switch (sign) {
     case '+':
-      printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+      printf("%.1d+%.1d=%.1d", first, second, first+second);
       break;
     case '-':
-      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+      printf("%.1d-%.1d=%.1d", first, second, first-second);
       break;
     case '*':
-      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+      printf("%.1d*%.1d=%.1d", first, second, first*second);
       break;
     case '/':
-      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+      printf("%.1d/%.1d=%.1d", first, second, first/second);
       break;
       default:
-      printf("Error! operator is not correct");
+      printf("the operator you have inserted is in correct.");
       getch();
   }
 
